c12616a6a84e0b046c327c96929558eed9df98d4
johnpao1010 <ingjohnduran@gmail.com>
2025-10-19 13:53:23 -0500

    se hacen cambios en cors para que permita acceso desde url de amplify

--- src/main/java/com/Deteccion_estrabismo/backend/Config/CorsConfig.java
+++ src/main/java/com/Deteccion_estrabismo/backend/Config/CorsConfig.java
@@ -10,12 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:19006",          // frontend web local
-                            "exp://192.168.0.5:8080",        // Expo Go local
-                            "http://detecteye.us-east-2.elasticbeanstalk.com" // producción
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                .allowedOrigins("http://localhost:19006", // frontend web local
+                        "exp://192.168.0.5:8080", // Expo Go local
+                        "https://feature-loginconexion.diuzv3k23w2bz.amplifyapp.com" // producción
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
